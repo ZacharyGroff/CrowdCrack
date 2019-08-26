4485faf5d4b46340fb38fc1b3b65f350723f8a1d
ZacharyGroff <groff.zackm@gmail.com>
2019-08-26 07:55:19 -0400

    update main

--- main.go
+++ main.go
@@ -1,13 +1,6 @@
 package main
 
-import (
-	"fmt"
-	"crypto/sha1"
-)
-
 func main() {
-	hasher := sha1.New()
-	password := []byte("Hello World")
-	hasher.Write(password)
-	fmt.Printf("%x\n", hasher.Sum(nil))
+	client := InitializeClient()
+	client.Start()
 }
