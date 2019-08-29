da65922352780834803527a9af351768b8736ea2
ZacharyGroff <groff.zackm@gmail.com>
2019-08-29 08:06:40 -0400

    add password queue

---
+++ queue/password_queue.go
@@ -0,0 +1,43 @@
+package queue
+
+import (
+	"errors"
+	"fmt"
+	"github.com/ZacharyGroff/CrowdCrack/config"
+)
+
+type PasswordQueue struct {
+	passwords chan string
+	config *config.ServerConfig
+}
+
+func NewPasswordQueue(config *config.ServerConfig) *PasswordQueue {
+	passwords := make(chan string, config.QueueBuffer)
+	return &PasswordQueue{passwords, config}
+}
+
+func (q PasswordQueue) Size() int {
+	return len(q.passwords)
+}
+
+func (q PasswordQueue) Get() (string, error) {
+	for {
+		select {
+		case password := <- q.passwords:
+			return password, nil
+		default:
+			err := errors.New("No Urls in queue.")
+			return "", err
+		}
+	}
+}
+
+func (q PasswordQueue) Put(password string) error {
+	select {
+	case q.passwords <- password:
+		return nil
+	default:
+		err := fmt.Errorf("No room in buffer. Discarding password: %q\n", password)
+		return err
+	}
+}
