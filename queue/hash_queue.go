6b95486c918b5e05217456184e7c152c049b69e6
ZacharyGroff <groff.zackm@gmail.com>
2019-08-30 08:12:45 -0400

    add HashQueue

---
+++ queue/hash_queue.go
@@ -0,0 +1,89 @@
+package queue
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+	"github.com/ZacharyGroff/CrowdCrack/config"
+)
+
+type HashQueue struct {
+	hashes chan string
+	config *config.Config
+}
+
+func NewHashQueue(config *config.ServerConfig) *HashQueue {
+	hashes := make(chan string, config.HashBuffer)
+	return &HashQueue{hashes, config}
+}
+
+func (q HashQueue) Size() int {
+	return len(q.hashes)
+}
+
+func (q HashQueue) Get() (string, error) {
+	for {
+		select {
+		case hash := <- q.hashes:
+			return hash, nil
+		default:
+			err := errors.New("No Urls in queue.")
+			return "", err
+		}
+	}
+}
+
+func (q HashQueue) Put(hash string) error {
+	select {
+	case q.hashes <- hash:
+		return nil
+	default:
+		err := fmt.Errorf("No room in buffer. Discarding hash: %+v\n", hash)
+		return err
+	}
+}
+
+func (q HashQueue) Flush() error {
+	if q.config.FlushToFile {
+		return q.flushToFile()
+	}
+	_, err := q.emptyChannel()
+	
+	return err
+}
+
+func (q HashQueue) flushToFile() error {
+	file, err := os.OpenFile(q.config.HashPath, os.O_WRONLY, os.ModePerm)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	writer := bufio.NewWriter(file)
+	hashes, err := q.emptyChannel()
+	if err != nil {
+		return err
+	}
+
+	for _, hash := range hashes {
+		fmt.Fprintln(writer, hash.String()) 
+	}
+
+	return writer.Flush()
+}
+
+func (q HashQueue) emptyChannel() ([]string, error) {
+	initialSize := len(q.hashes)
+	var hashes []string
+	for i := 0; i < initialSize; i++ {
+		hash, err := q.Get()
+		if err != nil {
+			return nil, err
+		}
+
+		hashes = append(hashes, hash)
+	}
+
+	return hashes, nil
+}
