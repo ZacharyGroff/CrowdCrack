6bc5d8fb6b55e163649ce6c7c71dd29b19020b22
ZacharyGroff <groff.zackm@gmail.com>
2019-09-01 14:01:59 -0400

    rename function

--- api/api.go
+++ api/api.go
@@ -21,11 +21,11 @@ func NewApi(c *config.ServerConfig, p *queue.PasswordQueue, h *queue.HashQueue) 
 
 func (a Api) HandleRequests() {
 	log.Printf("Api listening to requests on port %d", a.Config.ApiPort)
-	http.HandleFunc("/current-hash", a.getHash)
+	http.HandleFunc("/current-hash", a.getHashName)
 	port := fmt.Sprintf(":%d", a.Config.ApiPort)
 	log.Fatal(http.ListenAndServe(port, nil))
 }
 
-func (a Api) getHash(w http.ResponseWriter, r *http.Request) {
+func (a Api) getHashName(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(a.Config.HashFunction)
 }
