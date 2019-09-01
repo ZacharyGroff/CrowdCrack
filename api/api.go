8043492efed39d2b0ba32f9b513687bd82f41287
ZacharyGroff <groff.zackm@gmail.com>
2019-09-01 14:36:44 -0400

    hashes post route on api

--- api/api.go
+++ api/api.go
@@ -22,10 +22,27 @@ func NewApi(c *config.ServerConfig, p *queue.PasswordQueue, h *queue.HashQueue) 
 func (a Api) HandleRequests() {
 	log.Printf("Api listening to requests on port %d", a.Config.ApiPort)
 	http.HandleFunc("/current-hash", a.getHashName)
+	http.HandleFunc("/hashes", a.retrieveHashes)
 	port := fmt.Sprintf(":%d", a.Config.ApiPort)
 	log.Fatal(http.ListenAndServe(port, nil))
 }
 
 func (a Api) getHashName(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(a.Config.HashFunction)
+}
+
+func (a Api) retrieveHashes(w http.ResponseWriter, r *http.Request) {
+	var hashSubmission HashSubmission
+	decoder := json.NewDecoder(r.Body)
+
+	err := decoder.Decode(&hashSubmission)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for _, hash := range hashSubmission.Results {
+		for a.Hashes.Put(hash) != nil {}
+	}
+	
+	json.NewEncoder(w).Encode("Submission Successful")
 }
