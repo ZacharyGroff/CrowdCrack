429dcb5c1b82627d7560671be5657eb1e1b167d8
ZacharyGroff <groff.zackm@gmail.com>
2019-09-03 08:13:22 -0400

    read wordlist in server.Start

--- server/server.go
+++ server/server.go
@@ -4,18 +4,25 @@ import (
 	"log"
 	"github.com/ZacharyGroff/CrowdCrack/api"
 	"github.com/ZacharyGroff/CrowdCrack/config"
+	"github.com/ZacharyGroff/CrowdCrack/reader"
 )
 
 type Server struct {
 	Config *config.ServerConfig
 	Api *api.Api
+	Reader reader.PasswordReader
 }
 
-func NewServer(c *config.ServerConfig, a *api.Api) Server {
-	return Server{c, a}
+func NewServer(c *config.ServerConfig, a *api.Api, r *reader.WordlistReader) Server {
+	return Server{c, a, r}
 }
 
 func (s Server) Start() {
 	log.Println("Starting Server...")
+	err := s.Reader.LoadPasswords()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	s.Api.HandleRequests()
 }
