4306b123dd4d839a7f323e144197cb7449331463
ZacharyGroff <groff.zackm@gmail.com>
2019-09-04 07:55:41 -0400

    add LoadPasswords from wordlist

--- reader/wordlist_reader.go
+++ reader/wordlist_reader.go
@@ -1,6 +1,8 @@
 package reader
 
 import (
+	"bufio"
+	"os"
 	"github.com/ZacharyGroff/CrowdCrack/config"
 	"github.com/ZacharyGroff/CrowdCrack/queue"
 )
@@ -15,5 +17,24 @@ func NewWordlistReader(c *config.ServerConfig, p *queue.PasswordQueue) *Wordlist
 }
 
 func (w WordlistReader) LoadPasswords() error {
+	file, err := os.Open(w.config.WordlistPath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		password := scanner.Text()
+		err := w.passwords.Put(password)
+		if err != nil {
+			break
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		return err
+	}
+	
 	return nil
 }
