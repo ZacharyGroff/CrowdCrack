942c362565767594239f19f1cc6dc26b2f9ce58c
ZacharyGroff <groff.zackm@gmail.com>
2019-09-05 12:12:07 -0400

    add additional testing to WordlistReader

--- reader/wordlist_reader_test.go
+++ reader/wordlist_reader_test.go
@@ -2,7 +2,6 @@ package reader
 
 import (
 	"bufio"
-	"fmt"
 	"os"
 	"testing"
 	"github.com/ZacharyGroff/CrowdCrack/config"
@@ -20,7 +19,7 @@ func (m mockQueue) Get() (string, error) {
 	return "", nil
 }
 
-func (m mockQueue) Put(password string) error {
+func (m *mockQueue) Put(password string) error {
 	m.PutCalls++
 	return nil
 }
@@ -30,8 +29,10 @@ func setupWordlist(testPath string, passwords []string) {
 	defer file.Close()
 	writer := bufio.NewWriter(file)
 	for _, password := range passwords {
-		fmt.Fprintln(writer, password) 
+		writer.WriteString(password)
+		writer.WriteString("\n")
 	}
+	writer.Flush()
 }
 
 func TestLoadPasswordsNoError(t *testing.T) {
@@ -46,8 +47,63 @@ func TestLoadPasswordsNoError(t *testing.T) {
 	err := reader.LoadPasswords()
 	
 	if err != nil {
-		os.Remove(testPath)
 		t.Errorf("Unexpected error returned: %s\n", err.Error())
+	}
+	
+	os.Remove(testPath)
+}
+
+func TestLoadPasswordsError(t *testing.T) {
+	testPath := "wordlist_test.txt"
+
+	config := config.ServerConfig{WordlistPath: testPath}
+	queue := mockQueue{PutCalls: 0}
+	reader := WordlistReader{config: &config, passwords: &queue}
+
+	err := reader.LoadPasswords()
+	
+	if err == nil {
+		t.Error("Expected error but nil returned")
+	}
+}
+
+func TestLoadPasswordsPutNoCalls(t *testing.T) {
+	expected := uint64(0)
+
+	testPath := "wordlist_test.txt"
+	passwords := []string{}
+	setupWordlist(testPath, passwords)
+
+	config := config.ServerConfig{WordlistPath: testPath}
+	queue := mockQueue{PutCalls: 0}
+	reader := WordlistReader{config: &config, passwords: &queue}
+
+	reader.LoadPasswords()
+	
+	actual := queue.PutCalls
+	if actual != expected {
+		t.Errorf("Expected: %d\nActual: %d\n", expected, actual)
+	}
+
+	os.Remove(testPath)
+}
+
+func TestLoadPasswordsPutMultipleCalls(t *testing.T) {
+	expected := uint64(2)
+
+	testPath := "wordlist_test.txt"
+	passwords := []string{"password1", "password2"}
+	setupWordlist(testPath, passwords)
+
+	config := config.ServerConfig{WordlistPath: testPath}
+	queue := mockQueue{PutCalls: 0}
+	reader := WordlistReader{config: &config, passwords: &queue}
+
+	reader.LoadPasswords()
+	
+	actual := queue.PutCalls
+	if actual != expected {
+		t.Errorf("Expected: %d\nActual: %d\n", expected, actual)
 	}
 
 	os.Remove(testPath)
