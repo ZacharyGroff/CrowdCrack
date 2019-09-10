a629ab1c7a22bcbe05e49988106d4b9bf326080c
ZacharyGroff <groff.zackm@gmail.com>
2019-09-10 08:12:24 -0400

    hash out client.Start

--- client/client.go
+++ client/client.go
@@ -4,21 +4,33 @@ import (
 	"log"
 	"github.com/ZacharyGroff/CrowdCrack/config"
 	"github.com/ZacharyGroff/CrowdCrack/encoder"
+	"github.com/ZacharyGroff/CrowdCrack/requester"
+	"github.com/ZacharyGroff/CrowdCrack/submitter"
 )
 
 type Client struct {
 	config *config.ClientConfig
 	encoder encoder.Encoder
+	requester requester.Requester
+	submitter submitter.Submitter
 }
 
-func NewClient(c *config.ClientConfig, e *encoder.Hasher) Client {
-	return Client{c, e}
+func NewClient(c *config.ClientConfig, e *encoder.Hasher, r *requester.PasswordRequester, s *submitter.HashSubmitter) Client {
+	return Client{c, e, r, s}
 }
 
-func (client Client) Start() {
+func (c Client) Start() {
 	log.Println("Starting Client...")
+	for {
+		hash, err := c.requester.Request()
+		if err != nil {
+			log.Println(err)
+		}
+		c.encoder.Encode(hash)
+		c.submitter.Submit()
+	}
 }
 
-func (client Client) Stop() {
+func (c Client) Stop() {
 	log.Println("Stopping Client...")
 }
