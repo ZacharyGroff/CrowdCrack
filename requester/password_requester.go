9a77c833db8daf9a819903962eccf7709a0521ac
ZacharyGroff <groff.zackm@gmail.com>
2019-09-11 07:55:03 -0400

    add PasswordRequester GetHash

--- requester/password_requester.go
+++ requester/password_requester.go
@@ -1,8 +1,11 @@
 package requester
 
 import (
+	"fmt"
 	"hash"
 	"crypto/sha256"
+	"encoding/json"
+	"net/http"
 	"github.com/ZacharyGroff/CrowdCrack/config"
 	"github.com/ZacharyGroff/CrowdCrack/queue"
 )
@@ -10,12 +13,57 @@ import (
 type PasswordRequester struct {
 	config *config.ClientConfig
 	passwords queue.Queue
+	supportedHashes map[string]hash.Hash
 }
 
 func NewPasswordRequester(c *config.ClientConfig, q *queue.PasswordQueue) *PasswordRequester {
-	return &PasswordRequester{c, q}
+	s := getSupportedHashes()
+	return &PasswordRequester{c, q, s}
+}
+
+func getSupportedHashes() map[string]hash.Hash {
+	return map[string]hash.Hash {
+		"sha256": sha256.New(),
+	}
 }
 
 func (p PasswordRequester) Request() (hash.Hash, error) {
-	return sha256.New(), nil
+	hash, err := p.requestHash()
+
+	if err != nil {
+		return nil, err
+	}
+
+	passwords, err := 
+
+	return hash, nil
+}
+
+func (p PasswordRequester) getHash() (hash.Hash, error) {
+	hashName, err := p.requestHashName()
+	if err != nil {
+		return err
+	}
+
+	currentHash, isSupported := p.supportedHashes[hashName]
+	if !isSupported {
+		return nil, fmt.Errorf("Current hash: %s is unsupported.", hashName)
+	}
+
+	return currentHash, nil
+}
+
+func (p PasswordRequester) requestHashName() (string, error) {
+	address := p.config.ServerAddress + "/current-hash"
+	r, err := http.Get(address)
+	if err != nil {
+		return nil, err
+	}
+	defer r.Body.Close()
+
+	var hashName string
+	decoder := json.NewDecoder(r.Body)
+	err = decoder.Decode(&hashName)
+
+	return hashName, err
 }
