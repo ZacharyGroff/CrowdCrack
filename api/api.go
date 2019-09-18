013ff6280a4bb3350ef1936cb815d3a947a4ecf1
Zachary Groff <groff.zackm@gmail.com>
2019-09-18 08:42:00 -0400

    fix bug in api

--- api/api.go
+++ api/api.go
@@ -43,7 +43,7 @@ func (a Api) retrieveHashes(w http.ResponseWriter, r *http.Request) {
 	}
 
 	for _, hash := range hashSubmission.Results {
-		for a.Hashes.Put(hash) != nil {}
+		for a.Hashes.Put(hash) == nil {}
 	}
 	
 	json.NewEncoder(w).Encode("Submission Successful")
