5968d12c5f4a11aaf3d36336ddf211fcb09873e4
ZacharyGroff <groff.zackm@gmail.com>
2019-09-23 07:52:22 -0400

    finish testing server

--- server/server_test.go
+++ server/server_test.go
@@ -30,6 +30,24 @@ func assertHandleRequestsCalled(t *testing.T, a *mockApi) {
 	}
 }
 
+func assertVerifyNotCalled(t *testing.T, v *mockVerifier) {
+	time.Sleep(100 * time.Millisecond)
+	expected := uint64(0)
+	actual := v.verifyCalls
+	if expected != actual {
+		t.Errorf("Expected: %d\nActual: %d\n", expected, actual)
+	}
+}
+
+func assertHandleRequestsNotCalled(t *testing.T, a *mockApi) {
+	expected := uint64(0)
+	actual := a.handleRequestsCalls
+	if expected != actual {
+		t.Errorf("Expected: %d\nActual: %d\n", expected, actual)
+	}
+}
+
+
 func TestServerStartNoError(t *testing.T) {
 	mockApi := mockApi{0}
 	mockPasswordReader := mockPasswordReader{0, false}
@@ -41,4 +59,21 @@ func TestServerStartNoError(t *testing.T) {
 	assertLoadPasswordsCalled(t, &mockPasswordReader)
 	assertVerifyCalled(t, &mockVerifier)
 	assertHandleRequestsCalled(t, &mockApi)
+}
+
+func TestServerStartLoadPasswordsError(t *testing.T) {
+	mockApi := mockApi{0}
+	mockPasswordReader := mockPasswordReader{0, true}
+	mockVerifier := mockVerifier{0}
+	server := Server{&mockApi, &mockPasswordReader, &mockVerifier}
+
+	defer func() {
+		if r := recover(); r != nil {
+			assertLoadPasswordsCalled(t, &mockPasswordReader)
+			assertVerifyNotCalled(t, &mockVerifier)
+			assertHandleRequestsNotCalled(t, &mockApi)
+		}
+	}()
+	
+	server.Start()
 }
