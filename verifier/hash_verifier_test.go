5d94fdda748def1053d278fe2626af89f19f1b60
ZacharyGroff <groff.zackm@gmail.com>
2019-09-27 12:20:34 -0400

    finish testing hash verifier

--- verifier/hash_verifier_test.go
+++ verifier/hash_verifier_test.go
@@ -3,6 +3,7 @@ package verifier
 import (
 	"errors"
 	"reflect"
+	"strings"
 	"testing"
 	"github.com/ZacharyGroff/CrowdCrack/mocks"
 )
@@ -45,5 +46,68 @@ func TestHashVerifierLoadUserProvidedHashesError(t *testing.T) {
 	err := hashVerifier.loadUserProvidedHashes()
 	if err == nil {
 		t.Error("Expected error but nil returned")
+	}
+}
+
+func TestHashVerifierGetNextPasswordHashCorrectHash(t *testing.T) {
+	expected := "fakeHash"
+	var errToReturn error
+	mockFlushingQueue := mocks.NewMockFlushingQueue(expected, errToReturn)
+	hashVerifier := HashVerifier{computedHashes: &mockFlushingQueue}
+
+	actual := hashVerifier.getNextPasswordHash()
+	if strings.Compare(expected, actual) != 0 {
+		t.Errorf("Expected: %s\nActual: %s\n", expected, actual)
+	}
+}
+
+func TestHashVerifierParsePasswordHashCorrectPassword(t *testing.T) {
+	expected := "fakePassword"
+	hash := "fakeHash"
+	passwordHash := expected + ":" + hash
+	hashVerifier := HashVerifier{}
+
+	actual, _ := hashVerifier.parsePasswordHash(passwordHash)
+	if strings.Compare(expected, actual) != 0 {
+		t.Errorf("Expected: %s\nActual: %s\n", expected, actual)
+	}
+}
+
+func TestHashVerifierParsePasswordHashCorrectHash(t *testing.T) {
+	password := "fakePassword"
+	expected := "fakeHash"
+	passwordHash := password + ":" + expected
+	hashVerifier := HashVerifier{}
+
+	_, actual := hashVerifier.parsePasswordHash(passwordHash)
+	if strings.Compare(expected, actual) != 0 {
+		t.Errorf("Expected: %s\nActual: %s\n", expected, actual)
+	}
+}
+
+func TestHashVerifierIsMatchTrue(t *testing.T) {
+	expected := true
+	hash := "fakeHash"
+	userProvidedHashes := map[string]bool {
+		hash: true,
+	}
+	hashVerifier := HashVerifier{userProvidedHashes: userProvidedHashes}
+
+	actual := hashVerifier.isMatch(hash)
+	if expected != actual {
+		t.Errorf("Expected: %t\nActual: %t\n", expected, actual)
+	}
+}
+
+func TestHashVerifierIsMatchFalse(t *testing.T) {
+	expected := false
+	userProvidedHashes := map[string]bool {
+		"sha256": true,
+	}
+	hashVerifier := HashVerifier{userProvidedHashes: userProvidedHashes}
+
+	actual := hashVerifier.isMatch("fakeHash")
+	if expected != actual {
+		t.Errorf("Expected: %t\nActual: %t\n", expected, actual)
 	}
 }
