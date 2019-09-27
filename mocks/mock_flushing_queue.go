4934652fd274deb5bc6aa356f51704ee6293fd32
ZacharyGroff <groff.zackm@gmail.com>
2019-09-27 12:08:53 -0400

    add mock flushing queue

---
+++ mocks/mock_flushing_queue.go
@@ -0,0 +1,34 @@
+package mocks
+
+type MockFlushingQueue struct {
+	SizeCalls uint64
+	GetCalls uint64
+	PutCalls uint64
+	FlushCalls uint64
+	stringToReturn string
+	errorToReturn error
+}
+
+func NewMockFlushingQueue(s string, e error) MockFlushingQueue {
+	return MockFlushingQueue{0, 0, 0, 0, s, e}
+}
+
+func (m *MockFlushingQueue) Size() int {
+	m.SizeCalls++
+	return int(m.PutCalls) - int(m.GetCalls)
+}
+
+func (m *MockFlushingQueue) Get() (string, error) {
+	m.GetCalls++
+	return m.stringToReturn, m.errorToReturn
+}
+
+func (m *MockFlushingQueue) Put(s string) error {
+	m.PutCalls++
+	return m.errorToReturn
+}
+
+func (m *MockFlushingQueue) Flush() error {
+	m.FlushCalls++
+	return m.errorToReturn
+}
