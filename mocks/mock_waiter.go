7c7d0d2b5991591158d126a396091126ee859f0f
ZacharyGroff <groff.zackm@gmail.com>
2019-10-01 12:38:13 -0400

    cleanup hash submitter tests

--- mocks/mock_waiter.go
+++ mocks/mock_waiter.go
@@ -4,6 +4,10 @@ type MockWaiter struct {
 	WaitCalls uint64
 }
 
+func NewMockWaiter() MockWaiter {
+	return MockWaiter{0}
+}
+
 func (m *MockWaiter) Wait() {
 	m.WaitCalls++
 }
--- submitter/hash_submitter_test.go
+++ submitter/hash_submitter_test.go
@@ -7,125 +7,123 @@ import (
 	"github.com/ZacharyGroff/CrowdCrack/models"
 )
 
-func TestProcessSubmissionSuccess(t *testing.T) {
+type testObject struct {
+	hashSubmitter *HashSubmitter
+	mockSubmissionQueue *mocks.MockSubmissionQueue
+	mockApiClient *mocks.MockApiClient
+	mockWaiter *mocks.MockWaiter
+}
+
+func setupHashSubmitterForNoError() testObject {
 	mockSubmissionQueue := mocks.NewMockSubmissionQueue(error(nil), models.HashSubmission{})
 	mockApiClient := mocks.NewMockApiClient(200, "fakeHash", []string{})
-	hashSubmitter := HashSubmitter{submissionQueue: &mockSubmissionQueue, client: &mockApiClient}
+	mockWaiter := mocks.NewMockWaiter()
+	hashSubmitter := HashSubmitter{submissionQueue: &mockSubmissionQueue, client: &mockApiClient, waiter: &mockWaiter}
 
-	err := hashSubmitter.processSubmission()
+	return testObject{&hashSubmitter, &mockSubmissionQueue, &mockApiClient, &mockWaiter}
+}
+
+func setupHashSubmitterForClientError() testObject {
+	mockSubmissionQueue := mocks.NewMockSubmissionQueue(error(nil), models.HashSubmission{})
+	mockApiClient := mocks.NewMockApiClient(500, "fakeHash", []string{})
+	mockWaiter := mocks.NewMockWaiter()
+	hashSubmitter := HashSubmitter{submissionQueue: &mockSubmissionQueue, client: &mockApiClient, waiter: &mockWaiter}
+
+	return testObject{&hashSubmitter, &mockSubmissionQueue, &mockApiClient, &mockWaiter}
+}
+
+func setupHashSubmitterForSubmissionQueueError() testObject {
+	mockSubmissionQueue := mocks.NewMockSubmissionQueue(errors.New("test error"), models.HashSubmission{})
+	mockApiClient := mocks.NewMockApiClient(200, "fakeHash", []string{})
+	mockWaiter := mocks.NewMockWaiter()
+	hashSubmitter := HashSubmitter{submissionQueue: &mockSubmissionQueue, client: &mockApiClient, waiter: &mockWaiter}
+
+	return testObject{&hashSubmitter, &mockSubmissionQueue, &mockApiClient, &mockWaiter}
+}
+
+func assertClientSubmitHashesCalled(t *testing.T, testObject testObject) {
+	expected := uint64(1)
+	actual := testObject.mockApiClient.SubmitHashesCalls
+	if expected != actual {
+		t.Errorf("Expected %d\nActual: %d\n", expected, actual)
+	}
+}
+
+func assertSubmissionQueueGetCalled(t *testing.T, testObject testObject) {
+	expected := uint64(1)
+	actual := testObject.mockSubmissionQueue.GetCalls
+	if expected != actual {
+		t.Errorf("Expected %d\nActual: %d\n", expected, actual)
+	}
+}
+
+func assertClientSubmitHashesNotCalled(t *testing.T, testObject testObject) {
+	expected := uint64(0)
+	actual := testObject.mockApiClient.SubmitHashesCalls
+	if expected != actual {
+		t.Errorf("Expected %d\nActual: %d\n", expected, actual)
+	}
+}
+
+func TestProcessSubmissionSuccess(t *testing.T) {
+	testObject := setupHashSubmitterForNoError()
+
+	err := testObject.hashSubmitter.processSubmission()
 	if err != nil {
 		t.Errorf("Unexpected error returned: %s\n", err.Error())
 	}
 }
 
 func TestProcessSubmissionSuccessCorrectSubmissionQueueCalls(t *testing.T) {
-	expected := uint64(1)
-
-	mockSubmissionQueue := mocks.NewMockSubmissionQueue(error(nil), models.HashSubmission{})
-	mockApiClient := mocks.NewMockApiClient(200, "fakeHash", []string{})
-	hashSubmitter := HashSubmitter{submissionQueue: &mockSubmissionQueue, client: &mockApiClient}
-
-	hashSubmitter.processSubmission()
-
-	actual := mockSubmissionQueue.GetCalls
-	if expected != actual {
-		t.Errorf("Expected %d\nActual: %d\n", expected, actual)
-	}
+	testObject := setupHashSubmitterForNoError()
+	testObject.hashSubmitter.processSubmission()
+	assertSubmissionQueueGetCalled(t, testObject)
 }
 
 func TestProcessSubmissionSuccessCorrectClientCalls(t *testing.T) {
-	expected := uint64(1)
-
-	mockSubmissionQueue := mocks.NewMockSubmissionQueue(error(nil), models.HashSubmission{})
-	mockApiClient := mocks.NewMockApiClient(200, "fakeHash", []string{})
-	hashSubmitter := HashSubmitter{submissionQueue: &mockSubmissionQueue, client: &mockApiClient}
-
-	hashSubmitter.processSubmission()
-
-	actual := mockApiClient.SubmitHashesCalls
-	if expected != actual {
-		t.Errorf("Expected %d\nActual: %d\n", expected, actual)
-	}
+	testObject := setupHashSubmitterForNoError()
+	testObject.hashSubmitter.processSubmission()
+	assertClientSubmitHashesCalled(t, testObject)
 }
 
 func TestProcessSubmissionClientBadStatusCodeReturned(t *testing.T) {
-	mockSubmissionQueue := mocks.NewMockSubmissionQueue(error(nil), models.HashSubmission{})
-	mockApiClient := mocks.NewMockApiClient(500, "fakeHash", []string{})
-	hashSubmitter := HashSubmitter{submissionQueue: &mockSubmissionQueue, client: &mockApiClient}
+	testObject := setupHashSubmitterForClientError()
 
-	err := hashSubmitter.processSubmission()
+	err := testObject.hashSubmitter.processSubmission()
 	if err == nil {
 		t.Error("Expected error but nil returned")
 	}
 }
 
 func TestProcessSubmissionClientBadStatusCodeReturnedCorrectSubmissionQueueCalls(t *testing.T) {
-	expected := uint64(1)
-
-	mockSubmissionQueue := mocks.NewMockSubmissionQueue(error(nil), models.HashSubmission{})
-	mockApiClient := mocks.NewMockApiClient(500, "fakeHash", []string{})
-	hashSubmitter := HashSubmitter{submissionQueue: &mockSubmissionQueue, client: &mockApiClient}
-
-	hashSubmitter.processSubmission()
-
-	actual := mockSubmissionQueue.GetCalls
-	if expected != actual {
-		t.Errorf("Expected %d\nActual: %d\n", expected, actual)
-	}
+	testObject := setupHashSubmitterForClientError()
+	testObject.hashSubmitter.processSubmission()
+	assertSubmissionQueueGetCalled(t, testObject)
 }
 
 func TestProcessSubmissionClientBadStatusCodeReturnedCorrectClientCalls(t *testing.T) {
-	expected := uint64(1)
-
-	mockSubmissionQueue := mocks.NewMockSubmissionQueue(error(nil), models.HashSubmission{})
-	mockApiClient := mocks.NewMockApiClient(500, "fakeHash", []string{})
-	hashSubmitter := HashSubmitter{submissionQueue: &mockSubmissionQueue, client: &mockApiClient}
-
-	hashSubmitter.processSubmission()
-
-	actual := mockApiClient.SubmitHashesCalls
-	if expected != actual {
-		t.Errorf("Expected %d\nActual: %d\n", expected, actual)
-	}
+	testObject := setupHashSubmitterForClientError()
+	testObject.hashSubmitter.processSubmission()
+	assertClientSubmitHashesCalled(t, testObject)
 }
 
 func TestProcessSubmissionSubmissionQueueErrorReturned(t *testing.T) {
-	mockSubmissionQueue := mocks.NewMockSubmissionQueue(errors.New("test error"), models.HashSubmission{})
-	mockApiClient := mocks.NewMockApiClient(500, "fakeHash", []string{})
-	hashSubmitter := HashSubmitter{submissionQueue: &mockSubmissionQueue, client: &mockApiClient}
+	testObject := setupHashSubmitterForSubmissionQueueError()
 
-	err := hashSubmitter.processSubmission()
+	err := testObject.hashSubmitter.processSubmission()
 	if err == nil {
 		t.Error("Expected error but nil returned")
 	}
 }
 
 func TestProcessSubmissionSubmissionQueueErrorCorrectSubmissionQueueCalls(t *testing.T) {
-	expected := uint64(1)
-
-	mockSubmissionQueue := mocks.NewMockSubmissionQueue(errors.New("test error"), models.HashSubmission{})
-	mockApiClient := mocks.NewMockApiClient(500, "fakeHash", []string{})
-	hashSubmitter := HashSubmitter{submissionQueue: &mockSubmissionQueue, client: &mockApiClient}
-
-	hashSubmitter.processSubmission()
-
-	actual := mockSubmissionQueue.GetCalls
-	if expected != actual {
-		t.Errorf("Expected %d\nActual: %d\n", expected, actual)
-	}
+	testObject := setupHashSubmitterForSubmissionQueueError()
+	testObject.hashSubmitter.processSubmission()
+	assertSubmissionQueueGetCalled(t, testObject)
 }
 
 func TestProcessSubmissionSubmissionQueueErrorCorrectClientCalls(t *testing.T) {
-	expected := uint64(0)
-
-	mockSubmissionQueue := mocks.NewMockSubmissionQueue(errors.New("test error"), models.HashSubmission{})
-	mockApiClient := mocks.NewMockApiClient(500, "fakeHash", []string{})
-	hashSubmitter := HashSubmitter{submissionQueue: &mockSubmissionQueue, client: &mockApiClient}
-
-	hashSubmitter.processSubmission()
-
-	actual := mockApiClient.SubmitHashesCalls
-	if expected != actual {
-		t.Errorf("Expected %d\nActual: %d\n", expected, actual)
-	}
+	testObject := setupHashSubmitterForSubmissionQueueError()
+	testObject.hashSubmitter.processSubmission()
+	assertClientSubmitHashesNotCalled(t, testObject)
 }
