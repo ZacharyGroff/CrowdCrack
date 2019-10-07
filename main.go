401fc3beae2059266fc033657a460f6aa8e542ae
ZacharyGroff <groff.zackm@gmail.com>
2019-10-07 12:57:29 -0400

    update main

--- main.go
+++ main.go
@@ -1,9 +1,26 @@
 package main
 
+import (
+	"os"
+)
+
 func main() {
-	client := InitializeClient()
-	client.Start()
-	
+	args := os.Args[1:]
+
+	if isClient(args) {
+		client := InitializeClient()
+		client.Start()
+	}
+
 	server := InitializeServer()
 	server.Start()
+}
+
+func isClient(args []string) bool {
+	for _, b := range args {
+		if b == "--client" {
+			return true
+		}
+	}
+	return false
 }
