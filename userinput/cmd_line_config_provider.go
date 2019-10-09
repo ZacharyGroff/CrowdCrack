5677f5dd4988c98acc34ac4e668994ea3a409908
ZacharyGroff <groff.zackm@gmail.com>
2019-10-09 12:10:16 -0400

    fix bug in parsing cmd line

--- userinput/cmd_line_config_provider.go
+++ userinput/cmd_line_config_provider.go
@@ -8,25 +8,24 @@ import (
 type CmdLineConfigProvider struct {
 	clientConfig *models.ClientConfig
 	serverConfig *models.ServerConfig
-	isClient bool
 }
 
 func NewCmdLineConfigProvider() CmdLineConfigProvider {
-	clientConfig, serverConfig, isClient := parseCmdLine()
-	return CmdLineConfigProvider{clientConfig, serverConfig, isClient}
+	clientConfig, serverConfig := parseCmdLine()
+	return CmdLineConfigProvider{clientConfig, serverConfig}
 }
 
-func parseCmdLine() (*models.ClientConfig, *models.ServerConfig, bool) {
+func parseCmdLine() (*models.ClientConfig, *models.ServerConfig) {
 	serverAddressPtr := flag.String("a", "http://localhost:2725", "address of server to connect to")
-	hashQueueBufferPtr := flag.Uint64("hbc", 10000, "buffer size for hash queue")
-	passwordQueueBufferPtr := flag.Uint64("pbc", 10000, "buffer size for password queue")
-	flushToFilePtr := flag.Bool("fc", true, "flush computed hashes to file if hash buffer becomes full")
-	computedHashOverFlowPathPtr := flag.String("cpc", "output/computed_hash_overflow.txt", "path to file to flush computed hashes to")
+	hashQueueBufferPtr := flag.Uint64("hb", 10000, "buffer size for hash queue")
+	passwordQueueBufferPtr := flag.Uint64("pb", 10000, "buffer size for password queue")
+	flushToFilePtr := flag.Bool("f", true, "flush computed hashes to file if hash buffer becomes full")
+	computedHashOverFlowPathPtr := flag.String("cp", "output/computed_hash_overflow.txt", "path to file to flush computed hashes to")
 	wordListPathPtr := flag.String("wp", "wordlist.txt", "path to wordlist file")
 	hashListPathPtr := flag.String("hp", "hashlist.txt", "path to file containing hashes to crack")
 	hashFunctionPtr := flag.String("h", "sha256", "name of hash to use - currently supported: sha256")
 	apiPortPtr := flag.Uint("p", 2725, "port to expose for api")
-	isClientPtr := flag.Bool("client", false, "start application as client")
+	flag.Bool("client", false, "placeholder to allow checking of client arg in main")
 
 	flag.Parse()
 
@@ -48,11 +47,16 @@ func parseCmdLine() (*models.ClientConfig, *models.ServerConfig, bool) {
 		ComputedHashOverflowPath: *computedHashOverFlowPathPtr,
 	}
 
-	return clientConfig, serverConfig, *isClientPtr
+	return clientConfig, serverConfig
 }
 
-func (c *CmdLineConfigProvider) IsClient() bool {
-	return c.isClient
+func IsClient(args []string) bool {
+	for _, b := range args {
+		if b == "-client" {
+			return true
+		}
+	}
+	return false
 }
 
 func (c *CmdLineConfigProvider) GetClientConfig() *models.ClientConfig {
