6871a66c33c0aa9798bea4df728bf413a8d27d60
ZacharyGroff <groff.zackm@gmail.com>
2019-10-10 12:32:15 -0400

    add support for additional hashes

--- requester/password_requester.go
+++ requester/password_requester.go
@@ -3,8 +3,13 @@ package requester
 import (
 	"fmt"
 	"hash"
-	"crypto/sha256"
 	"crypto/md5"
+	"crypto/sha1"
+	"crypto/sha512"
+	"crypto/sha256"
+	"golang.org/x/crypto/md4"
+	"golang.org/x/crypto/ripemd160"
+	"golang.org/x/crypto/sha3"
 	"github.com/ZacharyGroff/CrowdCrack/apiclient"
 	"github.com/ZacharyGroff/CrowdCrack/models"
 	"github.com/ZacharyGroff/CrowdCrack/queue"
@@ -29,8 +34,18 @@ func NewPasswordRequester(p userinput.CmdLineConfigProvider, cl *apiclient.HashA
 
 func getSupportedHashes() map[string]hash.Hash {
 	return map[string]hash.Hash {
+		"md4": md4.New(),
 		"md5": md5.New(),
+		"sha1": sha1.New(),
 		"sha256": sha256.New(),
+		"sha512": sha512.New(),
+		"ripemd160": ripemd160.New(),
+		"sha3_224": sha3.New224(),
+		"sha3_256": sha3.New256(),
+		"sha3_384": sha3.New384(),
+		"sha3_512": sha3.New512(),
+		"sha512_224": sha512.New512_224(),
+		"sha512_256": sha512.New512_256(),
 	}
 }
 
