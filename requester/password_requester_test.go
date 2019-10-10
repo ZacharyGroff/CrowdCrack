30e195f66308f942985d3e16291dd312bed78816
ZacharyGroff <groff.zackm@gmail.com>
2019-10-10 12:37:49 -0400

    fix password requester test

--- requester/password_requester_test.go
+++ requester/password_requester_test.go
@@ -6,7 +6,12 @@ import (
 	"strings"
 	"testing"
 	"crypto/md5"
+	"crypto/sha1"
 	"crypto/sha256"
+	"crypto/sha512"
+	"golang.org/x/crypto/md4"
+	"golang.org/x/crypto/ripemd160"
+	"golang.org/x/crypto/sha3"
 	"github.com/ZacharyGroff/CrowdCrack/mocks"
 	"github.com/ZacharyGroff/CrowdCrack/models"
 )
@@ -57,7 +62,18 @@ func setupRequestQueueFull() mocks.MockRequestQueue {
 
 func setupSupportedHashes() map[string]hash.Hash {
 	return map[string]hash.Hash {
+		"md4": md4.New(),
+		"md5": md5.New(),
+		"sha1": sha1.New(),
 		"sha256": sha256.New(),
+		"sha512": sha512.New(),
+		"ripemd160": ripemd160.New(),
+		"sha3_224": sha3.New224(),
+		"sha3_256": sha3.New256(),
+		"sha3_384": sha3.New384(),
+		"sha3_512": sha3.New512(),
+		"sha512_224": sha512.New512_224(),
+		"sha512_256": sha512.New512_256(),
 	}
 }
 
@@ -182,10 +198,7 @@ func assertApiClientGetPasswordsCalled(t *testing.T, testObject testObject) {
 }
 
 func TestGetSupportedHashesCorrectHashes(t *testing.T) {
-	expected := map[string]hash.Hash {
-		"md5": md5.New(),
-		"sha256": sha256.New(),
-	}
+	expected := setupSupportedHashes()
 	actual := getSupportedHashes()
 	if !reflect.DeepEqual(expected, actual) {
 		t.Errorf("Expected: %+v\nActual: %+v\n", expectedHash, actual)
