93f4d0816622017c2b26c8c0b1b7d74f6aa1552f
ZacharyGroff <groff.zackm@gmail.com>
2019-10-14 12:31:13 -0400

    change logger interface to only log

--- logger/logger.go
+++ logger/logger.go
@@ -1,12 +1,5 @@
 package logger
 
-import (
-	"time"
-)
-
 type Logger interface {
-	LogPasswordsSent(uint64, time.Duration)
-	LogHashesComputed(uint64, time.Duration)
-	LogHashesCracked(uint64, time.Duration)
 	LogMessage(string)
 }
