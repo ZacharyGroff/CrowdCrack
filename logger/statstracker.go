3ea41d964adbe9a82defcce7e424978dd8522079
ZacharyGroff <groff.zackm@gmail.com>
2019-10-14 12:31:56 -0400

    update tracker and implementation to provide Getters

--- logger/statstracker.go
+++ logger/statstracker.go
@@ -1,22 +1,31 @@
 package logger
 
 type StatsTracker struct {
-	PasswordsSent uint64
-	HashesComputed uint64
-	HashesCracked uint64
+	passwordsSent uint64
+	hashesComputed uint64
+	hashesCracked uint64
 }
 
 func (s *StatsTracker) TrackPasswordsSent(passwordsSent uint64) {
-	s.PasswordsSent += passwordsSent
+	s.passwordsSent += passwordsSent
 }
 
 func (s *StatsTracker) TrackHashesComputed(hashesComputed uint64) {
-	s.HashesComputed += hashesComputed
+	s.hashesComputed += hashesComputed
 }
 
 func (s *StatsTracker) TrackHashesCracked(hashesCracked uint64) {
-	s.HashesCracked += hashesCracked
+	s.hashesCracked += hashesCracked
 }
 
+func (s *StatsTracker) GetPasswordsSent() uint64 {
+	return s.passwordsSent
+}
 
+func (s *StatsTracker) GetHashesComputed() uint64 {
+	return s.hashesComputed
+}
 
+func (s *StatsTracker) GetHashesCracked() uint64 {
+	return s.hashesCracked
+}
--- logger/tracker.go
+++ logger/tracker.go
@@ -4,4 +4,7 @@ type Tracker interface {
 	TrackPasswordsSent(uint64)
 	TrackHashesComputed(uint64)
 	TrackHashesCracked(uint64)
+	GetPasswordsSent() uint64
+	GetHashesComputed() uint64
+	GetHashesCracked() uint64
 }
