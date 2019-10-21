808f8ecde0a45e19cd0479cb5646860b0b8d2663
ZacharyGroff <groff.zackm@gmail.com>
2019-10-21 12:08:48 -0400

    update sleeper to use logger

--- waiter/sleeper.go
+++ waiter/sleeper.go
@@ -1,24 +1,30 @@
 package waiter
 
 import (
-	"log"
+	"github.com/ZacharyGroff/CrowdCrack/logger"
 	"time"
 )
 
 type Sleeper struct {
+	logger logger.Logger
 	sleepDuration time.Duration
 	isLogging bool
 	logMessage string
 }
 
-func NewSleeper(sleepSeconds int, isLogging bool, logMessage string) Sleeper {
+func NewSleeper(sleepSeconds int, isLogging bool, logMessage string, logger *logger.GenericLogger) Sleeper {
 	sleepDuration := time.Duration(sleepSeconds) * time.Second
-	return Sleeper{sleepDuration, isLogging, logMessage}
+	return Sleeper {
+		logger:        logger,
+		sleepDuration: sleepDuration,
+		isLogging:     isLogging,
+		logMessage:    logMessage,
+	}
 }
 
 func (s Sleeper) Wait() {
 	if s.isLogging {
-		log.Println(s.logMessage)
+		s.logger.LogMessage(s.logMessage)
 	}
 	time.Sleep(s.sleepDuration)
 }
