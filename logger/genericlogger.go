a5618c0b3bd60bf262df8d3a85ea56686333807d
ZacharyGroff <groff.zackm@gmail.com>
2019-10-22 08:14:47 -0400

    date format log messages

--- logger/genericlogger.go
+++ logger/genericlogger.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 	"github.com/ZacharyGroff/CrowdCrack/models"
 	"github.com/ZacharyGroff/CrowdCrack/userinput"
 )
@@ -30,6 +31,7 @@ func (s *GenericLogger) LogMessage(logMessage string) error {
 }
 
 func (s *GenericLogger) logToFile(logMessage string) error {
+	timeFormattedMessage := getTimeFormattedMessage(time.Now(), logMessage)
 	file, err := os.OpenFile(s.config.LogPath, os.O_WRONLY | os.O_CREATE | os.O_APPEND, os.ModePerm)
 	if err != nil {
 		return err
@@ -37,7 +39,12 @@ func (s *GenericLogger) logToFile(logMessage string) error {
 	defer file.Close()
 
 	writer := bufio.NewWriter(file)
-	fmt.Fprintln(writer, logMessage)
+	fmt.Fprintln(writer, timeFormattedMessage)
 
 	return writer.Flush()
+}
+
+func getTimeFormattedMessage(currentTime time.Time, logMessage string) string {
+	timeFormatted := currentTime.Format(time.RFC822)
+	return fmt.Sprintf("%s: %s", timeFormatted, logMessage)
 }
