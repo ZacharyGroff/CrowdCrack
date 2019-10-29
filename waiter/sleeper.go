02e02e28166f4ea9d7fc78db43deb5f76a13d59f
ZacharyGroff <groff.zackm@gmail.com>
2019-10-29 08:07:23 -0400

    update sleeper

--- waiter/sleeper.go
+++ waiter/sleeper.go
@@ -1,7 +1,10 @@
 package waiter
 
 import (
+	"fmt"
+	"runtime"
 	"time"
+	"github.com/ZacharyGroff/CrowdCrack/models"
 	"github.com/ZacharyGroff/CrowdCrack/logger"
 )
 
@@ -9,22 +12,28 @@ type Sleeper struct {
 	logger logger.Logger
 	sleepDuration time.Duration
 	isLogging bool
-	logMessage string
 }
 
-func NewSleeper(sleepSeconds int, isLogging bool, logMessage string, logger logger.Logger) Sleeper {
-	sleepDuration := time.Duration(sleepSeconds) * time.Second
+func NewSleeper(config *models.Config, logger logger.Logger) Sleeper {
+	sleepDuration := time.Duration(60) * time.Second
 	return Sleeper {
 		logger:        logger,
 		sleepDuration: sleepDuration,
-		isLogging:     isLogging,
-		logMessage:    logMessage,
+		isLogging:     config.Verbose,
 	}
 }
 
 func (s Sleeper) Wait() {
 	if s.isLogging {
-		s.logger.LogMessage(s.logMessage)
+		logMessage := s.getLogMessage()
+		s.logger.LogMessage(logMessage)
 	}
 	time.Sleep(s.sleepDuration)
 }
+
+func (s Sleeper) getLogMessage() string {
+	pc, _, _, _ := runtime.Caller(1)
+	details := runtime.FuncForPC(pc)
+	return fmt.Sprintf("%s sleeping for %d seconds", details.Name(), s.sleepDuration / time.Second)
+}
+
