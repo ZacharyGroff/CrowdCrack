968f294ddd681f5e6a37d12ee5062ccc16ac9204
ZacharyGroff <groff.zackm@gmail.com>
2019-11-01 12:37:54 -0400

    update tracker

--- tracker/statstracker.go
+++ tracker/statstracker.go
@@ -4,13 +4,15 @@ type StatsTracker struct {
 	passwordsSent uint64
 	hashesComputed uint64
 	hashesCracked uint64
+	hashMatchAttempts uint64
 }
 
 func NewStatsTracker() *StatsTracker {
 	return &StatsTracker{
-		passwordsSent:  0,
-		hashesComputed: 0,
-		hashesCracked:  0,
+		passwordsSent:     0,
+		hashesComputed:    0,
+		hashesCracked:     0,
+		hashMatchAttempts: 0,
 	}
 }
 
@@ -26,6 +28,10 @@ func (s *StatsTracker) TrackHashesCracked(hashesCracked uint64) {
 	s.hashesCracked += hashesCracked
 }
 
+func (s *StatsTracker) TrackHashMatchAttempt() {
+	s.hashesCracked++
+}
+
 func (s *StatsTracker) GetPasswordsSent() uint64 {
 	return s.passwordsSent
 }
@@ -35,5 +41,9 @@ func (s *StatsTracker) GetHashesComputed() uint64 {
 }
 
 func (s *StatsTracker) GetHashesCracked() uint64 {
+	return s.hashesCracked
+}
+
+func (s *StatsTracker) GetHashMatchAttempts() uint64 {
 	return s.hashesCracked
 }
