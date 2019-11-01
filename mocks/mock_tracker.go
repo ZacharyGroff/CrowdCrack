194575cc945b706f8a18d9b0c296649c26242d03
ZacharyGroff <groff.zackm@gmail.com>
2019-11-01 12:43:10 -0400

    update mock tracker

--- mocks/mock_tracker.go
+++ mocks/mock_tracker.go
@@ -4,21 +4,25 @@ type MockTracker struct {
 	TrackPasswordsSentCalls uint64
 	TrackHashesComputedCalls uint64
 	TrackHashesCrackedCalls uint64
+	TrackHashMatchAttemptCalls uint64
 	GetPasswordsSentCalls uint64
 	GetHashesComputedCalls uint64
 	GetHashesCrackedCalls uint64
+	GetHashMatchAttemptsCalls uint64
 	uint64ToReturn uint64
 }
 
 func NewMockTracker(u uint64) MockTracker {
 	return MockTracker{
-		TrackPasswordsSentCalls:  0,
-		TrackHashesComputedCalls: 0,
-		TrackHashesCrackedCalls:  0,
-		GetPasswordsSentCalls:    0,
-		GetHashesComputedCalls:   0,
-		GetHashesCrackedCalls:    0,
-		uint64ToReturn:           u,
+		TrackPasswordsSentCalls:    0,
+		TrackHashesComputedCalls:   0,
+		TrackHashesCrackedCalls:    0,
+		TrackHashMatchAttemptCalls: 0,
+		GetPasswordsSentCalls:      0,
+		GetHashesComputedCalls:     0,
+		GetHashesCrackedCalls:      0,
+		GetHashMatchAttemptsCalls:  0,
+		uint64ToReturn:             u,
 	}
 }
 
@@ -34,6 +38,10 @@ func (m *MockTracker) TrackHashesCracked(uint64) {
 	m.TrackHashesCrackedCalls++
 }
 
+func (m *MockTracker) TrackHashMatchAttempt() {
+	m.TrackHashMatchAttemptCalls++
+}
+
 func (m *MockTracker) GetPasswordsSent() uint64 {
 	m.GetPasswordsSentCalls++
 	return m.uint64ToReturn
@@ -46,5 +54,10 @@ func (m *MockTracker) GetHashesComputed() uint64 {
 
 func (m *MockTracker) GetHashesCracked() uint64 {
 	m.GetHashesCrackedCalls++
+	return m.uint64ToReturn
+}
+
+func (m *MockTracker) GetHashMatchAttempts() uint64 {
+	m.GetHashMatchAttemptsCalls++
 	return m.uint64ToReturn
 }
