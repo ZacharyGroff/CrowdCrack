997e90011ec4fc535a7209c19df7363cd5bac55f
ZacharyGroff <groff.zackm@gmail.com>
2019-11-07 07:29:35 -0500

    use encoder factory in client

--- client/client.go
+++ client/client.go
@@ -12,20 +12,20 @@ import (
 
 type Client struct {
 	config *models.Config
-	encoder encoder.Encoder
+	encoderFactory encoder.EncoderFactory
 	logger logger.Logger
 	requester requester.Requester
 	submitter submitter.Submitter
 }
 
-func NewClient(p userinput.CmdLineConfigProvider, e *encoder.Hasher, l *logger.ConcurrentLogger, r *requester.PasswordRequester, s *submitter.HashSubmitter) Client {
+func NewClient(p userinput.CmdLineConfigProvider, e *encoder.HasherFactory, l *logger.ConcurrentLogger, r *requester.PasswordRequester, s *submitter.HashSubmitter) Client {
 	c := p.GetConfig()
 	return Client{
-		config:    c,
-		encoder:   e,
-		logger:    l,
-		requester: r,
-		submitter: s,
+		config:           c,
+		encoderFactory:   e,
+		logger:           l,
+		requester:        r,
+		submitter:        s,
 	}
 }
 
@@ -35,32 +35,39 @@ func (c Client) Start() {
 	var wg sync.WaitGroup
 	wg.Add(3)
 
-	go func() {
-		err := c.requester.Start()
-		if err != nil {
-			c.logger.LogMessage(err.Error())
-		}
-		c.logger.LogMessage("Requester Done!")
-		wg.Done()
-	}()
-	go func() {
-		err := c.encoder.Start()
-		if err != nil {
-			c.logger.LogMessage(err.Error())
-		}
-		c.logger.LogMessage("Encoder Done!")
-		wg.Done()
-	}()
-	go func() {
-		err := c.submitter.Start()
-		if err != nil {
-			c.logger.LogMessage(err.Error())
-		}
-		c.logger.LogMessage("Submitter Done!")
-		wg.Done()
-	}()
+	go c.startRequester(&wg)
+	go c.startEncoder(&wg)
+	go c.startSubmitter(&wg)
 
 	wg.Wait()
+}
+
+func (c Client) startRequester(wg *sync.WaitGroup) {
+	err := c.requester.Start()
+	if err != nil {
+		c.logger.LogMessage(err.Error())
+	}
+	c.logger.LogMessage("Requester Done!")
+	wg.Done()
+}
+
+func (c Client) startEncoder(wg *sync.WaitGroup) {
+	encoder := c.encoderFactory.GetNewEncoder()
+	err := encoder.Start()
+	if err != nil {
+		c.logger.LogMessage(err.Error())
+	}
+	c.logger.LogMessage("Encoder Done!")
+	wg.Done()
+}
+
+func (c Client) startSubmitter(wg *sync.WaitGroup) {
+	err := c.submitter.Start()
+	if err != nil {
+		c.logger.LogMessage(err.Error())
+	}
+	c.logger.LogMessage("Submitter Done!")
+	wg.Done()
 }
 
 func (c Client) Stop() {
