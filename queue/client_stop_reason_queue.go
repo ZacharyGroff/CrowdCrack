ba16ffc952824fd863ddd44f5c5fcfcfe5347358
ZacharyGroff <groff.zackm@gmail.com>
2019-11-12 12:26:49 -0500

    implement ClientStopReasonQueue

---
+++ queue/client_stop_reason_queue.go
@@ -0,0 +1,46 @@
+package queue
+
+import (
+	"errors"
+	"fmt"
+	"github.com/ZacharyGroff/CrowdCrack/models"
+	"github.com/ZacharyGroff/CrowdCrack/userinput"
+)
+
+type ClientStopReasonQueue struct {
+	stopReasons chan models.ClientStopReason
+}
+
+func NewClientStopReasonQueue(p userinput.CmdLineConfigProvider) *ClientStopReasonQueue {
+	config := p.GetConfig()
+	stopReasons := getStopReasonsChannel(config)
+	return &ClientStopReasonQueue{
+		stopReasons: stopReasons,
+	}
+}
+
+func getStopReasonsChannel(config *models.Config) chan models.ClientStopReason {
+	buffer := config.Threads - 1
+	stopReasons := make(chan models.ClientStopReason, buffer)
+	return stopReasons
+}
+
+func (c ClientStopReasonQueue) Get() (models.ClientStopReason, error) {
+	select {
+	case reason := <-c.stopReasons:
+		return reason, nil
+	default:
+		err := errors.New("No reasons in queue.")
+		return models.ClientStopReason{}, err
+	}
+}
+
+func (c ClientStopReasonQueue) Put(reason models.ClientStopReason) error {
+	select {
+	case c.stopReasons <- reason:
+		return nil
+	default:
+		err := fmt.Errorf("No room in buffer. Discarding reason: %+v\n", reason)
+		return err
+	}
+}
