4cf33de177ea04816f7e0891e494b71dd287e099
ZacharyGroff <groff.zackm@gmail.com>
2019-12-09 14:26:34 -0500

    implement client backup reader

--- reader/client_backup_reader.go
+++ reader/client_backup_reader.go
@@ -1,8 +1,10 @@
 package reader
 
 import (
+	"encoding/json"
 	"github.com/ZacharyGroff/CrowdCrack/interfaces"
 	"github.com/ZacharyGroff/CrowdCrack/models"
+	"io/ioutil"
 	"os"
 )
 
@@ -28,7 +30,21 @@ func (c *ClientBackupReader) BackupsExist() bool {
 }
 
 func (c *ClientBackupReader) LoadBackups() error {
-	panic("implement me")
+	if fileExists(c.config.RequestBackupPath) {
+		err := c.loadRequests()
+		if err != nil {
+			return err
+		}
+	}
+
+	if fileExists(c.config.SubmissionBackupPath) {
+		err := c.loadSubmissions()
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 func fileExists(path string) bool {
@@ -38,4 +54,75 @@ func fileExists(path string) bool {
 	}
 
 	return false
+}
+
+func (c *ClientBackupReader) loadRequests() error {
+	jsonBytes, err := getJsonAsBytes(c.config.RequestBackupPath)
+	if err != nil {
+		return err
+	}
+
+	var requests []models.HashingRequest
+	json.Unmarshal(jsonBytes, &requests)
+
+	err = c.addRequestsToQueue(requests)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *ClientBackupReader) loadSubmissions() error {
+	jsonBytes, err := getJsonAsBytes(c.config.SubmissionBackupPath)
+	if err != nil {
+		return err
+	}
+
+	var submissions []models.HashSubmission
+	json.Unmarshal(jsonBytes, &submissions)
+
+	err = c.addSubmissionsToQueue(submissions)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *ClientBackupReader) addRequestsToQueue(requests []models.HashingRequest) error {
+	for _, request := range requests {
+		err := c.requestQueue.Put(request)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (c *ClientBackupReader) addSubmissionsToQueue(submissions []models.HashSubmission) error {
+	for _, submission := range submissions {
+		err := c.submissionQueue.Put(submission)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func getJsonAsBytes(path string) ([]byte, error) {
+	jsonFile, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer jsonFile.Close()
+
+	bytes, err := ioutil.ReadAll(jsonFile)
+	if err != nil {
+		return nil, err
+	}
+
+	return bytes, nil
 }
